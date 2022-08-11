27b3367577239b6f17ddddbab1dcc9b6d28bca92
devanshu <devanshu.kr.jha@gmail.com>
2022-08-11 23:03:20 +0530

    QueueMain.java added

---
+++ src/Stacks_Queues/QueueMain.java
@@ -0,0 +1,25 @@
+package Stacks_Queues;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+
+public class QueueMain {
+    public static void main(String[] args) throws Exception {
+//       // CustomQueue queue = new CustomQueue(4);
+//        DynamicQueue queue = new DynamicQueue();
+//        queue.insert(3);
+//        queue.insert(6);
+//        queue.insert(5);
+//        queue.insert(19);
+//        queue.insert(1);
+//
+//        queue.display();
+//        System.out.println(queue.remove());
+//        queue.display();
+        Queue<Integer> q = new ArrayDeque<>();
+        long[] ans;
+        Deque<Integer> dq = new ArrayDeque<>();
+
+    }
+}
